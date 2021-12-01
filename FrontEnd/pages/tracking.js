3c4d6976530991c559cc84009d743a95b3a94381
KaizukiZero <mypondalone@gmail.com>
2021-12-01 16:27:12 +0700

    Finish

--- FrontEnd/pages/tracking.js
+++ FrontEnd/pages/tracking.js
@@ -9,7 +9,7 @@ const coinstracking = () => {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=THB&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios.get(process.env.API)
     .then(res => {
       setCoins(res.data);
     }).catch(err => {
